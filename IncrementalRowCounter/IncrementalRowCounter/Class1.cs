6b94df65d9acd2064bc357fb57873b0518178576
michaelhansen079 <michaelhansen079@gmail.com>
2015-06-02 19:14:13 +0200

    adding skelet

---
+++ IncrementalRowCounter/IncrementalRowCounter/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SqlServer.Dts.Pipeline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementalRowCounter
+{
+    public class RowCounter : PipelineComponent
+    {
+
+    }
+}
